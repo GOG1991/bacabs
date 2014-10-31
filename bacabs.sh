3cbf1159a81a462ad922663298bbf56e27190654
Gerardo Ochoa <gochoa@tingsystems.com>
2014-10-31 13:49:39 -0600

    modifique bacabs......

--- bacabs.sh
+++ bacabs.sh
@@ -9,9 +9,9 @@ host='';
 nombre_usuario_directorio='';
 nombre_app='';
 nombre_dir_app='';
-nombre_user_bd='';
-passwd_userbd='';
-nombre_base_datos='';
+#nombre_user_bd='';
+#passwd_userbd='';
+#nombre_base_datos='';
 direccion_gitremoto_app='';
 puerto_nginx='';
 vercion_python='';
@@ -97,17 +97,15 @@ function Localisar()
 {
 	
 	Ldirectorio=$(find . -name $1 );
-	
 	if [ "$Ldirectorio" != "" ]
 	then 
 		
-		echo "Archivo localisado, se encuentra en el directorio $Ldirectorio"
+		echo "Archivo localisado, se encuentra en el directorio $Ldirectorio";
 			
 	else 
-		echo "El uarchivo $nombre_archivo no existe en el directorio" ;
+		echo "El uarchivo $nombre_archivo no existe en el directorio";
 		Exit 
 	fi
-
 }
 function ObtenerD()
 {
@@ -166,12 +164,11 @@ function Msenttings_py()
 	echo $staticroot;
 	mediaroot=$(sed -n -e '/MEDIA_ROOT/p' $loc_settings);
 	echo $mediaroot;
-	sed -e "s:$staticroot:STATIC_ROOT = '/home/$nombre_directorio_usuario/$nombre_entorno_virtual/static/':g" -e "s:$mediaroot:MEDIA_ROOT = '/home/$nombre_directorio_usuario/$nombre_entorno_virtual/static/':g" $loc_settings > config_app/settings.py;
+	sed -e "s:$staticroot:STATIC_ROOT = '/home/$nombre_directorio_usuario/$nombre_entorno_virtual/static/':g" -e "s:$mediaroot:MEDIA_ROOT = '/home/$nombre_directorio_usuario/$nombre_entorno_virtual/media/':g" $loc_settings > config_app/settings.py	
+	#sed -e "s:$staticroot:STATIC_ROOT = '/home/$nombre_directorio_usuario/$nombre_entorno_virtual/static/':g" -e "s:$mediaroot:MEDIA_ROOT = '/home/$nombre_directorio_usuario/$nombre_entorno_virtual/static/':g" $loc_settings > config_app/settings.py;
 	echo "Archivo settings.py configurado........";
 }
 ######################################
-
-
 #pedir parametros
 #limpiar................
 Limpiar_CrearD
@@ -179,7 +176,7 @@ Limpiar_CrearD
 function PIP()
 {
 	read -p "dame la ip del servidor: " host;
-	#host='192.168.1.50';
+	host='192.168.1.50';
 	ssh root@$host -p22 echo "conectando........";
 	if (( "$?" != "0" ))
 	then
@@ -222,7 +219,7 @@ function Nomapp()
 		echo "Nombre localisado, se encuentra en el directorio $Appdirectorio............"
 			
 	else 
-		echo "El Nombre $nombre_app de la alicasion no conincide. asegurese de que la aplicacion contenga la estrutura requerida........";
+		echo "El Nombre $nombre_app de la aplicasion no conincide. asegurese de que la aplicacion contenga la estrutura requerida........";
 		Exit 
 	fi
 	nombre_entorno_virtual="entorno$nombre_app";
@@ -257,25 +254,29 @@ function CreaUserDir()
 PideNUseryApp
 #crea usuario de app
 CreaUserDir 
+#pedir datos de la bd
+function PDBD()
+{
 
+	read -p "Dame el nombre del usuario de la base de datos: " nombre_user_bd;
+	nombre_user_bd='aser_adminbd2';
+	read -p "dame el passwd del usuario de la base de datos: " passwd_userbd;
+	passwd_userbd='JUvX8O08';
+	read -p "dame el nobre de la base de datos: " nombre_base_datos;
+	nombre_base_datos='aserdb080814';
 
-
-
-
-nombre_dir_app='aserprueba';#directorio que contine la app
-read -p "Dame el nombre del usuario de la base de datos: " nombre_user_bd;
-nombre_user_bd='aser_adminbd2';
-read -p "dame el passwd del usuario de la base de datos: " passwd_userbd;
-passwd_userbd='JUvX8O08';
-read -p "dame el nobre de la base de datos: " nombre_base_datos;
-nombre_base_datos='aserdb080814';
-
-read -p "Dame el puerto por el cual escucha Nginx: " puerto_nginx;
-puerto_nginx='80';
-read -p "Dame el numero de la vercion de Python pudeser 2.7, 3.0, 3.4 : " vercion_python;
-vercion_python='3.4';
-
-
+}
+#PDBD
+#pedir vercion de Python y pueto Nginx sshhh
+function PVercPNginxSsh()
+{
+	read -p "Dame el puerto por el cual escucha Nginx: " puerto_nginx;
+	puerto_nginx='80';
+	read -p "Dame el numero de la vercion de Python pudeser 2.7, 3.0, 3.4 : " vercion_python;
+	vercion_python='3.4';
+	read -p "Dame el puerto por el que se combiara ssh: " puerto_ssh;
+}
+PVercPNginxSsh
 #ssh $userservidor@$dominio -p$puerto 'apt-get update'
 ###############################################
 #modificar el archivo Cgunicor_statrt.sh y rediereccionarlo a carpeta config_r
@@ -286,37 +287,108 @@ Mapp_conf $nombre_app $nombre_directorio_usuario $nombre_entorno_virtual
 MNapp_conf $nombre_app $nombre_directorio_usuario $nombre_entorno_virtual $puerto_nginx $host
 
 #revisar que el archivo senttings 
-Rsenttings_py
+#Rsenttings_py
 #modificar el fichero senttings.py y redirecionarlo a config_app
 Msenttings_py
-#enviar el paquete al vps en el directorio/contenedor del usuario via ssh
-#descomprimir el paquete  via ssh
-#ejecutar bacabsser.sh via ssh
-#limpiar las carpetas recurso_app, config_app, config_r
-#rm -r -f mi_directorio la -f para que no pida confirmacion
-echo "limpiado drectorios config_r, recurso_app, y config_app........";
-#revizar el directorio donde esta ubicado antes de borara
-#rm -r -f config_r/*
-#rm -r -f recurso_app/*
-#rm -r -f config_app/*
-#mkdir recurso_app
-#aqui termina la funcion en el cliente y comiensa lo del servidor
-#actualiza el sistema
-#intala las depedencias y crea los enlaces necesarios
-#instala los servicios necesarios
-#crea la base de datos y su usuario
-#crea el entorno virtual con su vercion de python
-#se activa el entorno virtual y se mueve la app dentro de la carpeta del entorno 
-#se activa el entor virtual y se instalan los requrimens.txt
-#--------------------------
-#se crean los directorios necesarios si no estan primero se buscan
-#se crean los archivos nesezarios dentro de los directorios
-#se mueven los archivos de config_r a su lugar
-#se activan los servicios nesesarios y se configuran se reinician
-#se eliminan los archivos que se puedan de contenedor
-#reiniciar el servidor 
+#comensar con la actualizacion del sistema
+function ActualizarSO()
+{
+	ssh root@$host -p22 apt-get -y $1
+	if (( "$?" == "0" ))
+	then 
+		echo "........"
+	else
+		echo "el servicio no dio respuesta puede ser que notengas conexion a internet o que el servicio ya no tenga soporte";
+		Exit
+	fi
+}
+#ActualizarSO update
+#ActualizarSO upgrade
+#instalar dependencias
+function InstalarD()
+{
+	ssh root@$host -p22 apt-get -y install $1
+	if (( "$?" == "0" ))
+	then 
+	echo "la dependencia $1 se instalo correctamente"
+	else
+		echo "la dependencia no tubo respuesta, puede ser que notengas conexion a internet o que la dependencia ya no tenga soporte";
+		Exit
+	fi
+}
+#InstalarD libpq-dev
+#InstalarD python-dev
+#InstalarD python-setuptools
+#InstalarD python3-dev 
+#InstalarD python3-setuptools
+#InstalarD build-essential 
+#InstalarD python-imaging
+#InstalarD libjpeg-dev
+#InstalarD libjpeg8-dev
+#InstalarD libfreetype6
+#InstalarD libfreetype6-dev
+#InstalarD zlib1g-dev
+#InstalarD zlib1g-dev
 
-
-
-
-
+#crear enlaces sinbolicos
+function InstalaE()
+{
+	ssh root@$host -p22 ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
+	if (( "$?" == "0" ))
+	then 
+		echo "el enlase $1 se instalo correctamente";
+	else
+		echo "el enlase ya a sido creado";
+		
+	fi
+	ssh root@$host -p22 ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
+	if (( "$?" == "0" ))
+	then 
+		echo "el enlase $1 se instalo correctamente";
+	else
+		echo "el enlase ya a sido creado";
+		
+	fi
+	ssh root@$host -p22 ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
+	if (( "$?" == "0" ))
+	then 
+		echo "el enlase $1 se instalo correctamente";
+	else
+		echo "el enlase ya a sido creado";
+		
+	fi
+}
+#InstalaE 
+#instalar servicios 
+function InstalaS()
+{
+	ssh root@$host -p22 apt-get -y install $1
+	if (( "$?" == "0" ))
+	then 
+		echo "";
+	else
+		echo "el servicio no dio respuesta puede ser que notengas conexion a internet o que el servicio ya no tenga soporte";
+		#Exit
+	fi
+}
+	
+#InstalaS supervisor
+#InstalaS nginx
+#InstalaS postgresql 
+#InstalaS postgresql-contrib 
+#InstalaS git
+#InstalaS python-virtualenv 
+#InstalaS ufw 
+#configurar posgressql
+#remplasar archivo senttings por el modificado
+#crear entorno virtual 
+ssh root@$host -p22 virtualenv /home/$nombre_directorio_usuario/$nombre_entorno_virtual -p /usr/bin/python$vercion_python
+#activar entrono y mandar repositorio repositorio
+ssh root@$host -p22 source /home/$nombre_directorio_usuario/$nombre_entorno_virtual/bin/activate
+#scp -r recurso_app/* root@$host:/home/$nombre_directorio_usuario/$nombre_entorno_virtual
+#crear directorios dentro del entorno virtual
+ssh root@$host -p22 mkdir /home/$nombre_directorio_usuario/$nombre_entorno_virtual/logs
+ssh root@$host -p22 mkdir /home/$nombre_directorio_usuario/$nombre_entorno_virtual/run
+ssh root@$host -p22 mkdir /home/$nombre_directorio_usuario/$nombre_entorno_virtual/media
+ssh root@$host -p22 mkdir /home/$nombre_directorio_usuario/$nombre_entorno_virtual/static
+#/////////////////////
